43b0fc2da9c2dfa4b1f288801172c97cc9881f47
tenoenc <tenoenc@gmail.com>
2026-01-16 20:38:15 +0900

    feat(auth): UserRepository 인터페이스 구현

--- backend/mutr-app/src/main/java/com/teno/mutr/app/MutrAppApplication.java
+++ backend/mutr-app/src/main/java/com/teno/mutr/app/MutrAppApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.teno.mutr") // 모든 모듈의 빈(Service, Component 등) 스캔
 @EntityScan(basePackages = "com.teno.mutr") // 모든 모듈의 @Entity 스캔
+@EnableJpaRepositories(basePackages = "com.teno.mutr") // 모든 모듈의 Repository 스캔
 public class MutrAppApplication {
 
 	public static void main(String[] args) {
---
+++ backend/mutr-auth/src/main/java/com/teno/mutr/auth/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.teno.mutr.auth.repository;
+
+import com.teno.mutr.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByOauthId(String oauthId);
+}
